c7484bfceb753ddb89c9f45860f97acdb6eda101
DDavidescu <dbau43@yahoo.com>
2024-07-16 00:11:07 +0300

    GearQuest Clothing Website

---
+++ script.js
@@ -0,0 +1,60 @@
+// nav bar
+
+var menuItems = document.getElementById("menuItems");
+
+menuItems.style.maxHeight = "0px";
+
+function menuToggle(){
+    if(menuItems.style.maxHeight == "0px")
+    {
+        menuItems.style.maxHeight = "200px";
+    }
+    else
+    {
+        menuItems.style.maxHeight = "0px";
+    }
+}
+
+
+
+// toggle form
+
+var loginForm = document.getElementById("loginForm");
+var regForm = document.getElementById("regForm");
+var indicator = document.getElementById("indicator");
+
+function register(){
+    regForm.style.transform = "translateX(0px)";
+    loginForm.style.transform = "translateX(0px)";
+    indicator.style.transform = "translateX(100px)";
+}
+
+function login(){
+    regForm.style.transform = "translateX(300px)";
+    loginForm.style.transform = "translateX(300px)";
+    indicator.style.transform = "translateX(0px)";
+}
+
+
+
+// product gallery
+
+var productImg = document.getElementById("productImg");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function(){
+    productImg.src = smallImg[0].src;
+}
+
+smallImg[1].onclick = function(){
+    productImg.src = smallImg[1].src;
+}
+
+smallImg[2].onclick = function(){
+    productImg.src = smallImg[2].src;
+}
+
+smallImg[3].onclick = function(){
+    productImg.src = smallImg[3].src;
+}
+
